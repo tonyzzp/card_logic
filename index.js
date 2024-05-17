d5665e84b852ab4dda8be37df2df5a8eab101daa
tonyzzp <tonyzzp@qq.com>
2024-05-17 09:57:17 +0800

    jhandi munda

--- index.js
+++ index.js
@@ -27,10 +27,41 @@ function testCards(count) {
 
 }
 
-function main() {
-    for (let i = 1; i <= 10; i++) {
-        testCards(i)
+function testJhandiMunda() {
+    let TOTAL = 100000000
+    let totalWin = 0
+    for (let i = 0; i < TOTAL; i++) {
+        let results = [
+            Math.floor(Math.random() * 6),
+            Math.floor(Math.random() * 6),
+            Math.floor(Math.random() * 6),
+            Math.floor(Math.random() * 6),
+            Math.floor(Math.random() * 6),
+            Math.floor(Math.random() * 6),
+        ]
+        let count = results.reduce((rtn, current) => current == 0 ? rtn + 1 : rtn, 0)
+        let win = 0
+        if (count == 2) {
+            win = 3
+        } else if (count == 3) {
+            win = 5
+        } else if (count == 4) {
+            win = 10
+        } else if (count == 5) {
+            win = 20
+        } else if (count == 6) {
+            win = 100
+        }
+        totalWin += win
     }
+    console.info(TOTAL, totalWin, 1 - totalWin / TOTAL)
+}
+
+function main() {
+    // for (let i = 1; i <= 10; i++) {
+    //     testCards(i)
+    // }
+    testJhandiMunda()
 } 3
 
 main()
